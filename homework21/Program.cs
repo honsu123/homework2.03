1286ef0b55652c8950f50f801228dc8d6b3b3709
kirill <nicknamekirill@gmail.com>
2023-03-04 20:31:12 +0300

    complete

---
+++ homework21/Program.cs
@@ -0,0 +1,18 @@
+﻿Double x1, x2, y1, y2, z1, z2, distance;
+Console.WriteLine("1. Расстояние между двумя точками на плоскости.");
+Console.WriteLine("Выберите нужный пункт (1 или 2):");
+Console.Write("Введите координаты x1: ");
+x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты y1: ");
+y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты z1: ");
+z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты точки b:");
+Console.Write("Введите координаты x2: ");
+x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты y2: ");
+y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты z2: ");
+z2 = Convert.ToDouble(Console.ReadLine());
+distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)+ Math.Pow(z2 - z1, 2));
+Console.WriteLine("Расстояние между двумя точками в пространстве равно " + distance);
