bb49b4b642f26a2adc66965c0b70dc006fbe0e84
kirill <nicknamekirill@gmail.com>
2023-03-04 21:00:05 +0300

    complete

---
+++ homework19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = number;
+int result = 0;
+if (number>= 10000 && number <= 99999)
+while (number > 0)
+{
+    result *= 10;
+    result += number % 10;
+    number /= 10;
+}
+
+else
+Console.WriteLine("Число не является пятизначным");
+
+if (result == number2)
+    Console.WriteLine("Число является палиндромом");
+
+else Console.WriteLine("Число не является палиндромом");
+
